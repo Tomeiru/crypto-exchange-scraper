8c4868eabfefee6a29bd9898ae3c8c8afb5d6bd2
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-05-08 21:03:03 +0900

    feat: add tokens routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TokensController } from './tokens/tokens.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, TokensController],
   providers: [AppService],
 })
 export class AppModule {}
