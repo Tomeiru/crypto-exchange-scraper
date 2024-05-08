80b68971a48ee5544ff55f067741a73f43c2bdbc
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-05-08 20:05:33 +0900

    init(nest): pivot project to NestJS

--- handlers/tokens/create.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def handler(event, context):
-    return {
-        'statusCode': 201,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-    }
--- handlers/tokens/delete.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def handler(event, context):
-    return {
-        'statusCode': 204,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-    }
