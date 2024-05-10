2d377c3bdf29dc16937563fb9eba8fe2e04c55ad
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-05-10 17:31:20 +0900

    feat(TokensController): add GET route logic, add Rates deletion to DELETE route

--- src/tokens/tokens.module.ts
+++ src/tokens/tokens.module.ts
@@ -3,9 +3,10 @@ import { TokensController } from './tokens.controller';
 import { TokensService } from './tokens.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CoinMarketCapModule } from '../coin-market-cap/coin-market-cap.module';
+import { RatesModule } from '../rates/rates.module';
 
 @Module({
-  imports: [CoinMarketCapModule, PrismaModule],
+  imports: [CoinMarketCapModule, PrismaModule, RatesModule],
   controllers: [TokensController],
   providers: [TokensService],
   exports: [TokensService],
